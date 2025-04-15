bda289b88057aadd537f016821593e332fb8c92b
Vinaydeep Singh Padda <vinaydeepsinghpadda@Vinaydeeps-MacBook-Pro.local>
2025-04-15 11:36:07 -0400

    Project Started

---
+++ todo-ListApp/todo_ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_ListAppApp.swift
+//  todo-ListApp
+//
+//  Created by Vinaydeep Singh Padda on 2025-04-15.
+//
+
+import SwiftUI
+
+@main
+struct todo_ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
