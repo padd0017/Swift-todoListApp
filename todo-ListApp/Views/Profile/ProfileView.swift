5c3a9d45cbadec7d097fbd66dd8a1631c514a8a0
Vinaydeep Singh Padda <vinaydeepsinghpadda@Vinaydeeps-MacBook-Pro.local>
2025-04-17 22:24:58 -0400

    Solved error

--- todo-ListApp/Views/Profile/ProfileView.swift
+++ todo-ListApp/Views/Profile/ProfileView.swift
@@ -11,14 +11,18 @@ import SwiftUI
 
 
 struct ProfileView:  View {
-    var tasks: Tasks
+    var tasks: [Tasks]
     @EnvironmentObject var authManager: AuthManager
     @Environment(\.modelContext) private var context
     var body: some View {
         HStack{
             Button("Logout"){
                 Task {
-                    context.delete(tasks)
+                    for task in tasks{
+                        context.delete(task)
+                    }
+                    
+//                    context.delete([tasks])
                   _ =  try authManager.logout()
                 
                     
